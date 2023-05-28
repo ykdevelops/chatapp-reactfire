b99068954c3aef397657d284f6260dbffe3ab0c9
ykdevelops <ykdevelops@gmail.com>
2023-05-27 22:31:11 -0400

    feat: :art: More styling and added Emoji picker

--- src/components/AddMessage.js
+++ src/components/AddMessage.js
@@ -4,10 +4,13 @@ import '../App.css';
 import { BsFillSendFill } from 'react-icons/bs';
 import { CiMicrophoneOn } from 'react-icons/ci';
 import { BsEmojiSmile } from 'react-icons/bs';
+import Picker from 'emoji-picker-react';
+
 const MessagesCollection = collection(getFirestore(), 'messages');
 
 export default function AddMessage() {
     const [newMessage, setNewMessage] = useState('');
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
     const handleKeyDown = async (e) => {
         if (e.key === 'Enter') {
@@ -20,28 +23,39 @@ export default function AddMessage() {
         }
     };
 
+    const handleEmojiClick = (emojiObject) => {
+        const emoji = emojiObject.emoji;
+
+        setNewMessage((prevMessage) => prevMessage + emoji);
+        setShowEmojiPicker(false); // Close the emoji picker after selecting an emoji
+    };
+
     return (
-        <div className='addBoxRow'>
-            <div className='addBox'>
-                <button className="messageInputButton" >
+        <div className="addBoxRow">
+            <div className="addBox">
+                <button className="messageInputButton" onClick={() => setShowEmojiPicker(!showEmojiPicker)}>
                     <BsEmojiSmile className="messageInputIcon" />
                 </button>
                 <input
-                    className='addInput'
-                    type='text'
-                    placeholder='Type a message'
+                    className="addInput"
+                    type="text"
+                    placeholder="Type a message"
                     value={newMessage}
                     onChange={(e) => setNewMessage(e.target.value)}
                     onKeyDown={handleKeyDown}
                 />
-                <button className="messageInputButton" >
+                <button className="messageInputButton">
                     <CiMicrophoneOn className="messageInputIcon" />
                 </button>
-                <button className="messageInputButton" >
-                    <BsFillSendFill className="messageInputIcon" />
+                <button className="messageSendButton">
+                    <BsFillSendFill className="messageSendIcon" />
                 </button>
             </div>
+            {showEmojiPicker && (
+                <div className="emojiPickerContainer">
+                    <Picker onEmojiClick={handleEmojiClick} />
+                </div>
+            )}
         </div>
-
     );
 }
