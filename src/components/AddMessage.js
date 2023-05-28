d8340ffe1a8c695ec54f0ab28f5537304a38f86f
ykdevelops <ykdevelops@gmail.com>
2023-05-27 22:56:25 -0400

    feat: :sparkles: Added framer motion for message input and delete

--- src/components/AddMessage.js
+++ src/components/AddMessage.js
@@ -2,15 +2,12 @@ import React, { useState } from 'react';
 import { collection, addDoc, getFirestore } from 'firebase/firestore';
 import '../App.css';
 import { BsFillSendFill } from 'react-icons/bs';
-import { CiMicrophoneOn } from 'react-icons/ci';
 import { BsEmojiSmile } from 'react-icons/bs';
-import Picker from 'emoji-picker-react';
 
 const MessagesCollection = collection(getFirestore(), 'messages');
 
 export default function AddMessage() {
     const [newMessage, setNewMessage] = useState('');
-    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
     const handleKeyDown = async (e) => {
         if (e.key === 'Enter') {
@@ -23,17 +20,10 @@ export default function AddMessage() {
         }
     };
 
-    const handleEmojiClick = (emojiObject) => {
-        const emoji = emojiObject.emoji;
-
-        setNewMessage((prevMessage) => prevMessage + emoji);
-        setShowEmojiPicker(false); // Close the emoji picker after selecting an emoji
-    };
-
     return (
         <div className="addBoxRow">
             <div className="addBox">
-                <button className="messageInputButton" onClick={() => setShowEmojiPicker(!showEmojiPicker)}>
+                <button className="messageInputButton">
                     <BsEmojiSmile className="messageInputIcon" />
                 </button>
                 <input
@@ -44,16 +34,11 @@ export default function AddMessage() {
                     onChange={(e) => setNewMessage(e.target.value)}
                     onKeyDown={handleKeyDown}
                 />
-
                 <button className="messageSendButton">
                     <BsFillSendFill className="messageSendIcon" />
                 </button>
             </div>
-            {showEmojiPicker && (
-                <div className="emojiPickerContainer">
-                    <Picker onEmojiClick={handleEmojiClick} />
-                </div>
-            )}
         </div>
     );
 }
+
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { collection, deleteDoc, doc, query, orderBy } from 'firebase/firestore';
 import { AiOutlineDelete } from 'react-icons/ai';
 import { useFirestore, useFirestoreCollectionData } from 'reactfire';
+import { motion, AnimatePresence } from 'framer-motion';
 import '../App.css';
 
 export default function MessageList() {
@@ -14,6 +15,7 @@ export default function MessageList() {
 
     const [hoveredRow, setHoveredRow] = useState(null);
     const messagesEndRef = useRef(null);
+    const [newMessageId, setNewMessageId] = useState(null); // <-- Define newMessageId state
 
     const deleteMessage = async (messageId) => {
         try {
@@ -30,6 +32,10 @@ export default function MessageList() {
         }
     }, [messages]);
 
+    const handleNewMessage = (messageId) => {
+        setNewMessageId(messageId); // <-- Set newMessageId state
+    };
+
     if (status === 'loading') {
         return <p>Loading...</p>;
     }
@@ -40,23 +46,29 @@ export default function MessageList() {
 
     return (
         <ul className="messageList">
-            {messages.map((message, index) => (
-                <li
-                    key={message.id}
-                    className="messageRow"
-                    onMouseEnter={() => setHoveredRow(message.id)}
-                    onMouseLeave={() => setHoveredRow(null)}
-                >
-                    {hoveredRow === message.id && (
-                        <button className="messageDeleteIcon" onClick={() => deleteMessage(message.id)}>
-                            <AiOutlineDelete className="messageDeleteIcon" />
-                        </button>
-                    )}
-                    <div className="messageBubble">{message.text}</div>
-                    {index === messages.length - 1 && <div ref={messagesEndRef} />}
-                </li>
-            ))}
+            <AnimatePresence>
+                {messages.map((message) => (
+                    <motion.li
+                        key={message.id}
+                        className={`messageRow ${message.id === newMessageId ? 'newMessage' : ''}`}
+                        onMouseEnter={() => setHoveredRow(message.id)}
+                        onMouseLeave={() => setHoveredRow(null)}
+                        initial={{ opacity: 0, y: 10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: 10 }}
+                        transition={{ duration: 0.3 }}
+                    >
+
+                        {hoveredRow === message.id && (
+                            <button className="messageDeleteIcon" onClick={() => deleteMessage(message.id)}>
+                                <AiOutlineDelete className="messageDeleteIcon" />
+                            </button>
+                        )}
+                        <div className="messageBubble">{message.text}</div>
+                        {message.id === newMessageId && <div ref={messagesEndRef} />}
+                    </motion.li>
+                ))}
+            </AnimatePresence>
         </ul>
     );
 }
-
