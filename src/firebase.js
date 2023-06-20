f478abbb3aaa2029ae197d76067137b29ffeec18
ykdevelops <ykdevelops@gmail.com>
2023-06-20 14:05:57 -0400

    perf: :zap: created .env file for security of firestore details

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: "AIzaSyB_L0EwOnpVH4PUTkzUSpryQd-Kts2awPw",
-    authDomain: "chatapp-159ec.firebaseapp.com",
-    projectId: "chatapp-159ec",
-    storageBucket: "chatapp-159ec.appspot.com",
-    messagingSenderId: "39942104696",
-    appId: "1:39942104696:web:aedea9f37e365ed933af8d",
-    measurementId: "G-TCYTLGPD4T"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 initializeApp(firebaseConfig);
